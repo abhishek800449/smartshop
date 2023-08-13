c9f9c4abfa29f0663052f10f91061b6ab7c7603e
Abhishek Kumar Rai <abhishek800449@gmail.com>
2023-08-13 15:14:48 +0530

    Email Verification

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,15 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+# importing files for verification
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
@@ -15,12 +24,24 @@ def register(request):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             username = email.split("@")[0]
-
+            # creating user account
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration Successfull')
-            return redirect('register')
+            # Now activating user's account
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)), # we are encoding the primary key of user
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email,])
+            send_email.send()
+            # messages.success(request, 'Thank you for registration. We have sent you an activation link, to verify your account.')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:       
         form = RegistrationForm()
     context = {
@@ -50,3 +71,20 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation Link')
+        return redirect('register')
--- smartshop/settings.py
+++ smartshop/settings.py
@@ -145,3 +145,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587 # it is 587 for gmail and different for other hosts.
+EMAIL_HOST_USER = 'akr800449@gmail.com'
+EMAIL_HOST_PASSWORD = 'yjtmclzgrpuiavza' # this is app password created in google account
