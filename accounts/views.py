a2692ad957f9ca23c9d7845ed761a0f0d90ac8c1
Abhishek Kumar Rai <abhishek800449@gmail.com>
2023-08-13 21:27:14 +0530

    Forgot Password feature

--- accounts/views.py
+++ accounts/views.py
@@ -58,8 +58,8 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            # messages.success(request, 'You are now logged in')
-            return redirect('home')
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
@@ -88,3 +88,68 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid activation Link')
         return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+            # sending reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset your password'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)), # we are encoding the primary key of user
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email,])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+
+        else:
+            messages.error(request, 'Account does not exists')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/forgotPassword.html')
+
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid']=uid
+        messages.success(request, 'Please reset your password.')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'Invalid Link')
+        return redirect('login')
+    
+
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password==confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password) # this is build in function in django normal save() does not work for paswords.
+            user.save()
+            messages.success(request, 'Password reset successful.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match.')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
--- carts/views.py
+++ carts/views.py
@@ -91,6 +91,9 @@ def remove_cart_item(request, product_id, cart_item_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax=0
+        grand_total=0
+        total=0
         cart = Cart.objects.get(cart_id = _cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
