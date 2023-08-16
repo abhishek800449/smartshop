10d84fa2a5fc6cfe8ef9a5ed11db5483568612bb
Abhishek Kumar Rai <abhishek800449@gmail.com>
2023-08-16 21:32:08 +0530

    Ratings and Review

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from category.models import Category
+from accounts.models import Account
+from django.db.models import Avg, Count
 
 # Create your models here.
 
@@ -22,6 +24,20 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+
 
 class VariationManager(models.Manager):
     def colors(self):
@@ -37,6 +53,7 @@ variation_category_choice=(
     ('size', 'size'),
 )
 
+
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variation_category = models.CharField(max_length=100, choices=variation_category_choice)
@@ -48,3 +65,18 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+    
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
