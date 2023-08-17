7c4873e97571bf662222164ec3944bcf8511069a
Abhishek Kumar Rai <abhishek800449@gmail.com>
2023-08-18 01:43:56 +0530

    Product Gallery

--- smartshop/views.py
+++ smartshop/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product,  ReviewRating
 
 # Create your views here.
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
+
+    # Get the reviews
+    reviews = None
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
 
     context ={
         'products':products,
+        'reviews': reviews,
     }
     return render(request, 'index.html', context)
